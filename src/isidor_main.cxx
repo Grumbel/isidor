253ef6f915110b3409609263002f500111e32ff9
Ingo Ruhnke <grumbel@gmail.com>
2015-04-14 19:36:59 +0200

    Minor indention and copyright header cleanup

--- src/isidor_main.cxx
+++ src/isidor_main.cxx
@@ -1,21 +1,19 @@
-//  $Id$
+// Isidor
+// Copyright (C) 2002 Ingo Ruhnke <grumbel@gmail.com>
 //
-//  Isidor
-//  Copyright (C) 2002 Ingo Ruhnke <grumbel@gmx.de>
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License
-//  as published by the Free Software Foundation; either version 2
-//  of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <iostream>
 #include <ClanLib/core.h>
@@ -39,19 +37,19 @@ IsidorMain::main(int argc, char** argv)
   std::cout << "Isidor V0.0" << std::endl;
 
   try {
-  CL_SetupCore::init();
-  CL_SetupDisplay::init();
-  CL_SetupGL::init();
+    CL_SetupCore::init();
+    CL_SetupDisplay::init();
+    CL_SetupGL::init();
 
-  CL_DisplayWindow window("Isidor V0.0", 800, 600);
+    CL_DisplayWindow window("Isidor V0.0", 800, 600);
 
-  IsotileLayer layer1("wtest.png");
-  //IsotileLayer layer2("tilemap2.png");
-  //IsotileLayer layer3("tilemap3.png");
-  //IsotileLayer layer4("tilemap4.png");
-  IsowallLayer layer5("wtest2.png");
-  
-  while (!CL_Keyboard::get_keycode(CL_KEY_ESCAPE))
+    IsotileLayer layer1("wtest.png");
+    //IsotileLayer layer2("tilemap2.png");
+    //IsotileLayer layer3("tilemap3.png");
+    //IsotileLayer layer4("tilemap4.png");
+    IsowallLayer layer5("wtest2.png");
+
+    while (!CL_Keyboard::get_keycode(CL_KEY_ESCAPE))
     {
       CL_Display::clear();
       layer1.draw(0);
@@ -60,13 +58,13 @@ IsidorMain::main(int argc, char** argv)
       CL_System::keep_alive();
     }
 
-  CL_SetupGL::deinit();
-  CL_SetupDisplay::deinit();
-  CL_SetupCore::deinit();
+    CL_SetupGL::deinit();
+    CL_SetupDisplay::deinit();
+    CL_SetupCore::deinit();
   } catch(CL_Error& err) {
     std::cout << "CL_Error: " << err.message << std::endl;
   }
-  
+
   std::cout << "Thanks for using Isidor!" << std::endl;
   return 0;
 }
--- src/isotile_layer.cxx
+++ src/isotile_layer.cxx
@@ -1,21 +1,19 @@
-//  $Id$
+// Isidor
+// Copyright (C) 2002 Ingo Ruhnke <grumbel@gmail.com>
 //
-//  Pingus - A free Lemmings clone
-//  Copyright (C) 2002 Ingo Ruhnke <grumbel@gmx.de>
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
 //
-//  This program is free software; you can redistribute it and/or
-//  modify it under the terms of the GNU General Public License
-//  as published by the Free Software Foundation; either version 2
-//  of the License, or (at your option) any later version.
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
 //
-//  This program is distributed in the hope that it will be useful,
-//  but WITHOUT ANY WARRANTY; without even the implied warranty of
-//  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//  GNU General Public License for more details.
-//
-//  You should have received a copy of the GNU General Public License
-//  along with this program; if not, write to the Free Software
-//  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software
+// Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <iostream>
 #include <ClanLib/display.h>
@@ -36,10 +34,10 @@ IsotileLayer::IsotileLayer(std::string filename)
   tiles.push_back(CL_Surface("tile4.png"));
   tiles.push_back(CL_Surface("tile5.png"));
   tiles.push_back(CL_Surface("tile5.png"));
-      
+
   width  = buffer.get_width();
   height = buffer.get_height();
-  
+
   tilemap.resize(width * height);
 
   for(int y = 0; y < height; ++y)
@@ -64,37 +62,37 @@ void
 IsotileLayer::draw(int lheight)
 {
   for(int i = 0; i < height; ++i)
+  {
+    for(int j = 0; j < width; ++j)
     {
-      for(int j = 0; j < width; ++j)
+      int x = 32*(j-i);
+      int y = 16*(j+i);
+
+      if (tilemap[width * i + j])
+      {
+        CL_Surface& surf = tiles[tilemap[width * i + j]];
+
+        if (0)
         {
-          int x = 32*(j-i);
-          int y = 16*(j+i);
-          
-          if (tilemap[width * i + j])
-            {
-              CL_Surface& surf = tiles[tilemap[width * i + j]];
+          float dist = sqrt((i - height/2)*(i - height/2)
+                            + (j - width/2)*(j - width/2));
+          dist += lheight;
+          dist /= 16.0f;
+          if (dist > 1.0f)
+            dist = 1.0f;
 
-              if (0)
-                {
-              float dist = sqrt((i - height/2)*(i - height/2)
-                                + (j - width/2)*(j - width/2));
-              dist += lheight;
-              dist /= 16.0f;
-              if (dist > 1.0f)
-                dist = 1.0f;
-
-              surf.set_color(1.0f-dist,
-                             1.0f-dist,
-                             1.0f-dist,
-                             1.0f/(4.0f-lheight));
-                }
-              surf.draw(x + 320, y - surf.get_height() - lheight*24  + 100);
-            }
-          
-          //std::cout << tilemap[width * i + j] << " ";
+          surf.set_color(1.0f-dist,
+                         1.0f-dist,
+                         1.0f-dist,
+                         1.0f/(4.0f-lheight));
         }
-      //std::cout << std::endl;
+        surf.draw(x + 320, y - surf.get_height() - lheight*24  + 100);
+      }
+
+      //std::cout << tilemap[width * i + j] << " ";
     }
+    //std::cout << std::endl;
+  }
 }
 
 /* EOF */
